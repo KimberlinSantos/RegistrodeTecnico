9cc7ad4fa82277370c50381fb5d3d209e74ea669
KimberlinSantiago <Kimberlinsantiago0327@gmail.com>
2024-05-17 22:13:17 -0400

    Se agrego Tecnico

--- Components/Models/Tecnico.cs
+++ Components/Models/Tecnico.cs
@@ -1,12 +1,13 @@
-﻿namespace RegistrodeTecnico.Components.Models
-{
-    public class Tecnico
-    {
-        
-            public int TecnicoId { get; set; }
-            public string Nombres { get; set; }
-            public decimal SueldoHora { get; set; }
-        
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace RegistrodeTecnico.Components.Models
+{
+    public class TipoTecnico
+    {
+        public int TipoId { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        [StringLength(50, ErrorMessage = "La descripción no puede superar los 50 caracteres")]
+        public string Descripcion { get; set; }
     }
+
 }
