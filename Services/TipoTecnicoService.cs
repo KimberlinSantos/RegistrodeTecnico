d6df9498126ac69daed7e6ddb2d9f675215562d4
KimberlinSantiago <Kimberlinsantiago0327@gmail.com>
2024-05-17 22:21:15 -0400

    Se agrego TipoTecnicoService

---
+++ Services/TipoTecnicoService.cs
@@ -0,0 +1,55 @@
+﻿using RegistrodeTecnico.Components.Models;
+
+namespace RegistrodeTecnico.Services
+{
+    public class TipoTecnicoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TipoTecnicoService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<IEnumerable<TipoTecnico>> GetTiposTecnicos()
+        {
+            var response = await _httpClient.GetAsync("api/tipostecnico");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<IEnumerable<TipoTecnico>>();
+            }
+            else
+            {
+                throw new Exception("Error al obtener tipos técnicos");
+            }
+        }
+
+        public async Task AddTipoTecnico(TipoTecnico tipoTecnico)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/tipostecnico", tipoTecnico);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error al agregar tipo técnico");
+            }
+        }
+
+        public async Task UpdateTipoTecnico(TipoTecnico tipoTecnico)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/tipostecnico/{tipoTecnico.TipoId}", tipoTecnico);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error al actualizar tipo técnico");
+            }
+        }
+
+        public async Task DeleteTipoTecnico(int tipoId)
+        {
+            var response = await _httpClient.DeleteAsync($"api/tipostecnico/{tipoId}");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error al eliminar tipo técnico");
+            }
+        }
+    }
+
+}
