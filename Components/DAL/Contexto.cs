52006f4463f7dbe9c294fec015310c36ed3a4f91
KimberlinSantiago <Kimberlinsantiago0327@gmail.com>
2024-05-17 23:05:18 -0400

    Se agrego contexto

---
+++ Components/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrodeTecnico.Components.Pages;
+using RegistroTecnicos.Models;
+using System.Collections.Generic;
+namespace RegistroTecnicos.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options)
+        : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
